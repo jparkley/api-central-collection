8e8d9ffdb1d240c205ba38de3b2d857ffb7fb186
jparkley <parkleydondon@yahoo.com>
2024-03-27 19:04:57 -0700

    add scraper

--- expressjs-api-scraping-enter/index.js
+++ expressjs-api-scraping-enter/index.js
@@ -1,4 +1,4 @@
-const PORT = 8080;
+const PORT = process.env.port || 8080;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
@@ -7,6 +7,36 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+app.get("/enter", async (req, res) => {
+  try {
+    const content = await scraper();
+    return res.status(200).json({
+      result: content,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString(),
+    });
+  }
+});
+
+async function scraper() {
+  const content = [];
+  await axios.get("https://www.allkpop.com/").then((res) => {
+    const html = res.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("pop")', html).each(function () {
+      content.push({
+        title: $(this).text(),
+        url: $(this).attr("href"),
+      });
+    });
+  });
+  // console.log("🚀 ~ content:", content);
+  return content;
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
