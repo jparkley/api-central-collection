0494c28f08b923c333893b6e05f78461a5758a27
jparkley <parkleydondon@yahoo.com>
2024-04-06 12:36:17 -0700

    add more routes

--- expressjs-advanced/routes/index.js
+++ expressjs-advanced/routes/index.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/contacts").get((req, res) => {
-  res.status(200).json({ message: "getting contacts using router" });
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+router.route("/contacts").post((req, res) => {
+  res.status(200).json({ message: "Add new contact" });
+});
+
+router.route("/contacts/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+});
+
+router.route("/contacts/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+});
+
+router.route("/contacts/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
 });
 
 module.exports = router;
--- expressjs-advanced/server.js
+++ expressjs-advanced/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const router = require("./routes/index");
+const router = require("./routes/");
 
 const app = express();
 
