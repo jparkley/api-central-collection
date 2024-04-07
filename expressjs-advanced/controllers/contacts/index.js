87a4c31e661cf452d6cb2e3925910cd030a7d030
jparkley <parkleydondon@yahoo.com>
2024-04-06 21:16:49 -0700

    ExpressAdvanced: add model(ContactModel) to CRUD activity. Add get, create

--- expressjs-advanced/controllers/contacts/index.js
+++ expressjs-advanced/controllers/contacts/index.js
@@ -1,17 +1,34 @@
+const Contact = require("../../models/contactModel");
+
 // @route: GET /api/contacts
-const getContacts = (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+const getContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    // res.status(200).json({ message: "Get all contacts" });
+    res.status(200).json(contacts);
+  } catch (err) {
+    throw new Error("Error while fetching contacts");
+  }
 };
 
 // @route: POST /api/contacts
-const createContact = (req, res) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone) {
-    res.status(400);
-    throw new Error("All fields are required");
-  }
+const createContact = async (req, res) => {
+  // Use validation pipe ?
+  // const { name, email, phone } = req.body;
+  // if (!name || !email || !phone) {
+  //   res.status(400);
+  //   throw new Error("All fields are required");
+  // }
 
-  res.status(201).json({ message: "Add new contact" });
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await Contact.create({ name, email, phone });
+    console.log("🚀 ~ createContact ~ newContact:", newContact);
+    // res.status(201).json({ message: "Add new contact" });
+    res.status(201).json(newContact);
+  } catch (err) {
+    throw new Error("Error while creating a new contact");
+  }
 };
 // @route: GET /api/contacts/:id
 const getContact = (req, res) => {
