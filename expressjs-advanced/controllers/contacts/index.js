17959c10dbedd58e31191bad039c8fc49c2e7f4e
jparkley <parkleydondon@yahoo.com>
2024-04-09 11:01:15 -0700

    Add express-async-handler for error handling

--- expressjs-advanced/controllers/contacts/index.js
+++ expressjs-advanced/controllers/contacts/index.js
@@ -1,49 +1,40 @@
+const asyncHandler = require("express-async-handler");
 const Contact = require("../../models/contactModel");
 
 // @route: GET /api/contacts
-const getContacts = async (req, res) => {
-  try {
-    const contacts = await Contact.find();
-    // res.status(200).json({ message: "Get all contacts" });
-    res.status(200).json(contacts);
-  } catch (err) {
-    throw new Error("Error while fetching contacts");
-  }
-};
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  // res.status(200).json({ message: "Get all contacts" });
+  res.status(200).json(contacts);
+});
 
 // @route: POST /api/contacts
-const createContact = async (req, res) => {
-  // Use validation pipe ?
-  // const { name, email, phone } = req.body;
-  // if (!name || !email || !phone) {
-  //   res.status(400);
-  //   throw new Error("All fields are required");
-  // }
-
-  try {
-    const { name, email, phone } = req.body;
-    const newContact = await Contact.create({ name, email, phone });
-    console.log("🚀 ~ createContact ~ newContact:", newContact);
-    // res.status(201).json({ message: "Add new contact" });
-    res.status(201).json(newContact);
-  } catch (err) {
-    throw new Error("Error while creating a new contact");
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are required");
   }
-};
+  const newContact = await Contact.create({ name, email, phone });
+  console.log("🚀 ~ createContact ~ newContact:", newContact);
+  // res.status(201).json({ message: "Add new contact" });
+  res.status(201).json(newContact);
+});
+
 // @route: GET /api/contacts/:id
-const getContact = (req, res) => {
+const getContact = asyncHandler((req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 // @route: PUT /api/contacts/:id
-const updateContact = (req, res) => {
+const updateContact = asyncHandler((req, res) => {
   res.status(201).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
 // @route: DELETE /api/contacts/:id
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler((req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
