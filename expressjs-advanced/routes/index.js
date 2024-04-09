29612d69173ec01c0b10d08ca0d9dcf904468135
jparkley <parkleydondon@yahoo.com>
2024-04-09 11:29:01 -0700

    add new routes for users

--- expressjs-advanced/routes/index.js
+++ expressjs-advanced/routes/index.js
@@ -15,4 +15,16 @@ router.route("/contacts/:id").get(getContact);
 router.route("/contacts/:id").put(updateContact);
 router.route("/contacts/:id").delete(deleteContact);
 
+router.post("/users/register", (req, res) => {
+  res.json({ message: "User register here" });
+});
+
+router.post("/users/login", (req, res) => {
+  res.json({ message: "User Login here" });
+});
+
+router.get("/users/profile", (req, res) => {
+  res.json({ message: "Here is the user profile" });
+});
+
 module.exports = router;
